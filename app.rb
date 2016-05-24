158f8dd48ee9bf1d98f4dccb8ae4ff4dcf068bca
Am2200 <am.200@rambler.ru>
2016-05-24 21:18:08 +0300

    ssh test

--- app.rb
+++ app.rb
@@ -17,6 +17,9 @@ configure do
               )'
 end
 
+get '/showusers' do
+  
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
