10a4742905135d513d01daf20f42f998658978c1
Am2200 <am.200@rambler.ru>
2016-05-24 21:13:16 +0300

    testUnix

--- app.rb
+++ app.rb
@@ -17,10 +17,6 @@ configure do
               )'
 end
 
-def hello
-  p "123"
-end
-
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
